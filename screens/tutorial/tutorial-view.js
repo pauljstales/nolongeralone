57dc650ce5536ec94a5cc1d4f3b96ca30271f8a0
Paul J Stales <pauljstales@gmail.com>
2021-04-26 23:17:17 -0400

    changes

--- screens/tutorial/tutorial-view.js
+++ screens/tutorial/tutorial-view.js
@@ -4,3 +4,22 @@
  * @author Paul J Stales <https://twitter.com/pauljstales>
  * Copyright (c) 2021
  */
+
+var howToPlay = `<p class="screen__text screen__text--size--medium">
+          * Click a square on the battlefield grid to fire your energy weapon.
+        </p>
+        <p class="screen__text screen__text--size--medium">
+          * You have limited energy, and each shot takes 1 energy!
+        </p>
+        <p class="screen__text screen__text--size--medium">
+          * Click "weapons" to learn about the special weapons.
+        </p>
+        <p class="screen__text screen__text--size--medium">
+          * The grid appears empty because the aliens are cloaked... hitting
+          their ship will permanently reveal that part of the ship.
+        </p>
+        <p class="screen__text screen__text--size--medium">
+          * Alien ships can MOVE!
+        </p>`;
+
+var howToWin = ``;

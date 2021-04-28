2b452fdb235f64522eaba844351aef9213db5a29
Paul J Stales <pauljstales@gmail.com>
2021-04-27 23:37:51 -0400

    cleanup

--- screens/intro/intro-model.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview The view as part of the MVC design pattern, responsible for dealing with the UI.
- * @summary view object
- * @author Paul J Stales <https://twitter.com/pauljstales>
- * Copyright (c) 2021
- */
